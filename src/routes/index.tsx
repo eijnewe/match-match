290ecf6906d71e9e8e8a566b73e0ecd3230648b1
Alice Karlén <alice.karlen@hotmail.com>
2026-02-09 13:06:22 +0100

    Add routing to game, HowToPlay component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import { Link } from "@tanstack/react-router";
+import { HowToPlay } from "@/components/how-to-play";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
-  return <div>Hello "/"!</div>
+  return (
+    <div className="p-4 h-screen">
+      <div className="flex flex-col justify-center items-center h-full gap-4">
+        <Button>
+          <Link to="/game">Start game</Link>
+        </Button>
+        <HowToPlay />
+      </div>
+    </div>
+  );
 }
