3de828fb42d960cb05f204594f3d62b4bc1a3351
jprecenth <jenny.precenth@chasacademy.se>
2026-02-23 09:59:55 +0100

    Additional random changes

--- src/features/game/components/CategoryBanner.tsx
+++ src/features/game/components/CategoryBanner.tsx
@@ -8,16 +8,12 @@ export function CategoryBanner({
   categoryCount,
 }) {
   const isTwoRows = pinnedCategories.length > categoryCount / 2
-  console.log(categoryCount)
-  console.log(pinnedCategories)
-
-  console.log(isTwoRows)
 
   return (
     <div
       className={clsx(
-        'grid grid-flow-col gap-2 bg-border dark:bg-border overflow-x-auto items-stretch auto-rows-fr p-3 shrink-0',
-        isTwoRows ? 'grid-rows-2' : 'grid-rows-1',
+        'grid grid-flow-col gap-1 bg-border dark:bg-border absolute w-full left-0 p-1.5 overflow-x-auto items-stretch max-h-60 auto-rows-fr',
+        isTwoRows ? 'grid-rows-2' : 'grid-rows-1'
       )}
     >
       {pinnedCategories.map((cat) => (
@@ -25,13 +21,6 @@ export function CategoryBanner({
       ))}
       <CustomCard type='completedCategory' categoryTitle='Horses'/>
       <CustomCard type="plus"/>
-      {/* <span
-          key={cat}
-          className="px-2 py-1 bg-primary/10 rounded text-sm flex items-center gap-1"
-        >
-          {cat}
-          <button onClick={() => onRemoveCategory(cat)}>remove</button>
-        </span> */}
     </div>
   )
 }
